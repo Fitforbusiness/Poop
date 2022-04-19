98d4f958ec06be419d42d7ceda33b07299ce9bb4
Fitforbuisness <robs1well@gmail.com>
2022-04-19 10:30:57 -0700

    init

---
+++ main.py
@@ -0,0 +1,46 @@
+
+poop = "The poop says: where's the toilet?"
+print (poop.upper().isupper())
+toilet = "Toilet says: where's the paper?"
+print (len(toilet))
+phrase = "You got to love"
+print (phrase[1])
+scoop = "you are a noob"
+print (scoop.index("noob"))
+bear = "big boy"
+print (bear.replace("boy","girl"))
+for r in range (2):
+    print ("this is the baba")
+#25.0
+print ((2*(18+7))/2)
+print (type("5.2"))
+print ("There are " + str(5.2) + " poops in the toilet")
+print (5%4)
+# working on function from youtube
+def function1():
+    print ("ahhhh")
+    print ("ahhhhh 2")
+function1 ()
+# a mapping
+# input or an argument
+def function2 (x):
+    return 2*x
+a = function2 (3)
+#return value or output
+print (a)
+def function3 (b):
+    return 25/b
+pooping = function3 (5)
+print (pooping)
+sugar = function2 (5)
+print (sugar)
+help = function2 (10)
+print (help)
+def function10 (ph):
+    return 5+ph
+yo = function10 (20)
+print (yo)
+def newton (q):
+    return 5+q
+hey_there = newton (5)
+print (hey_there)
